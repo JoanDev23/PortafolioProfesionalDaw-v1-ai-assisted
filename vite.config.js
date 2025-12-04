c7c885211b8857eb2f4c24358f13b51a309ecad4
Joan elvis Jimenez ramos <joan.jimenez.dev@gmail.comgit config --global user.name JoanDev23>
2025-12-04 13:32:47 +0100

    Actualizar configuración y estructura de proyecto

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  root: './', // raíz del proyecto
+  build: {
+    outDir: 'dist', // carpeta de salida del build
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        sobreMi: path.resolve(__dirname, 'pages/sobre-mi.html'),
+        proyectos: path.resolve(__dirname, 'pages/proyectos.html'),
+        contacto: path.resolve(__dirname, 'pages/contacto.html')
+      }
+    }
+  }
+});
